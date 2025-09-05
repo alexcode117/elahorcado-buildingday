8f2ee95617450a37893f32a5771b85e163f006f8
Zedmous <59229407+Zedmous@users.noreply.github.com>
2025-09-05 16:22:14 -0400

    brack

--- src/logic/logic.js
+++ src/logic/logic.js
@@ -1,1 +1,16 @@
 //aqui va la logica
+
+export const actualizarPalabra = (posiciones=[], letra="") => {
+    semaforo=false;
+    actualizar=localStorage.getItem("").split("");
+    i=0;
+    while(i<posiciones.length){
+        if(posiciones[i]===letra){
+
+            semaforo=true;
+        }
+        i++;
+    }
+
+    return semaforo
+}
